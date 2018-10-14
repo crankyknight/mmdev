4d4c156d8fc963ada4e259bcdfa0d2885be06df8
Abhishek <abhi95kumar@gmail.com>
2018-10-14 23:17:45 +0530

    Added basic init for cdev

--- mmdev.c
+++ mmdev.c
@@ -16,28 +16,62 @@
 #include "mmdev.h"
 
 int mmdev_nr_dev = MMDEV_NR_DEVS;
-int mmdev_debug = 0;
+int mmdev_debug = 1;
 
-module_param(mmdev_nr_dev, int, 0644);
+module_param(mmdev_nr_dev, int, 0444);
 module_param(mmdev_debug, int, 0644);
 
 MODULE_AUTHOR("Abhishek Kumar");
-MODULE("Dual BSD/GPL");
+MODULE_LICENSE("Dual BSD/GPL");
 
 struct mmdev_dev *mmdev_devices;
+u32 mmdev_major;
+u32 mmdev_minor = 0;
+
+void mmdev_setup_cdev(struct mmdev_dev *mmdev, u32 index)
+{
+    u32 res;
+    dev_t dev = MKDEV(mmdev_major, mmdev_minor + index);
+
+    cdev_init(&mmdev->cdev, &mmdev_fops);
+    mmdev->cdev.owner = THIS_MODULE;
+    res = cdev_add(&mmdev->cdev, dev, 1);
+    if(res < 0)
+        KDBG("Unable to add cdev\n");
+}
+
+void mmdev_exit_module(void)
+{
+    KDBG("Module exit\n");
+}
 
 int mmdev_init_module(void)
 {
-    KDBG("Module init");
+    dev_t dev;
+    u32 result, i;
 
-    fail :
+    result = alloc_chrdev_region(&dev, mmdev_minor, mmdev_nr_devs);
+    if(result<0){
+        KDBG("Chardev allocation error");
+        return result;
+    }
+    mmdev_major = MAJOR(dev);
+   
+    mm_device = kmalloc(mmdev_nr_devs * sizeof(struct mmdev_dev));
+    if(!mm_device){
+        KDBG("Memory allocation error");
+        goto out;
+    }
+
+    for(i=0; i<mmdev_nr_devs; i++){
+        mm_device[i].mmdev_size = mmdev_size;
+        sema_init(&mm_device[i].sem, 1);
+        mmdev_setup_cdev(&mm_device[i], i); 
+    }
+
+    out :
         mmdev_exit_module();
         return 1;
-}
-
-void mmdev_exit_moduel(void)
-{
-    KDBG("Module exit");
 }
 
 module_init(mmdev_init_module);
--- mmdev.h
+++ mmdev.h
@@ -2,9 +2,22 @@
 #define _MMDEV_H_
 
 #define MMDEV_NR_DEVS   (4)
+#define MMDEV_SIZE      (4096)
+
 
 #ifdef __KERNEL__
-#   define KDBG(fmt, args...) printk(KERN_ALERT "mmdev : ", fmt, ##args)
+#   define KDBG(fmt, args...) \
+    do{ \
+        if(mmdev_debug & 0x01) \
+            printk(KERN_WARNING "mmdev : " fmt, ##args); \
+    }while(0)
+
 #endif
+
+struct mmdev_dev{
+    u32 mmdev_size;
+    struct cdev cdev;
+    struct semaphore sem;
+};
 
 #endif /*_MMDEV_H_*/
